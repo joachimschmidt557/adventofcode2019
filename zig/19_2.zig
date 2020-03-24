42e6b1e97ecf73fefd74aa92a577cd195fe0ea03
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-03-24 13:53:15 +0100

    add 19 part 2

---
+++ zig/19_2.zig
@@ -0,0 +1,409 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
+const AutoHashMap = std.AutoHashMap;
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+const ExecError = error{
+    InvalidOpcode,
+    InvalidParamMode,
+};
+
+fn opcode(ins: Instr) Instr {
+    return @rem(ins, 100);
+}
+
+test "opcode extraction" {
+    expectEqual(opcode(1002), 2);
+}
+
+fn paramMode(ins: Instr, pos: Instr) Instr {
+    var div: Instr = 100; // Mode of parameter 0 is in digit 3
+    var i: Instr = 0;
+    while (i < pos) : (i += 1) {
+        div *= 10;
+    }
+    return @rem(@divTrunc(ins, div), 10);
+}
+
+test "param mode extraction" {
+    expectEqual(paramMode(1002, 0), 0);
+    expectEqual(paramMode(1002, 1), 1);
+    expectEqual(paramMode(1002, 2), 0);
+}
+
+pub const Instr = i64;
+pub const Intcode = []Instr;
+
+pub const Mem = struct {
+    backend: ArrayList(Instr),
+
+    const Self = @This();
+
+    pub fn init(intcode: Intcode, alloc: *Allocator) Self {
+        return Self{
+            .backend = ArrayList(Instr).fromOwnedSlice(alloc, intcode),
+        };
+    }
+
+    pub fn get(self: Self, pos: usize) Instr {
+        return if (pos < self.backend.len) self.backend.at(pos) else 0;
+    }
+
+    pub fn set(self: *Self, pos: usize, val: Instr) !void {
+        if (pos < self.backend.len) {
+            self.backend.set(pos, val);
+        } else {
+            const old_len = self.backend.len;
+            try self.backend.resize(pos + 1);
+
+            var i: usize = old_len;
+            while (i < self.backend.len) : (i += 1) {
+                self.backend.set(i, 0);
+            }
+
+            self.backend.set(pos, val);
+        }
+    }
+};
+
+pub const IntcodeComputer = struct {
+    mem: Mem,
+    pc: usize,
+    relative_base: Instr,
+    state: State,
+    input: ?Instr,
+    output: ?Instr,
+
+    const Self = @This();
+
+    pub const State = enum {
+        Stopped,
+        Running,
+        AwaitingInput,
+        AwaitingOutput,
+    };
+
+    pub fn init(intcode: Intcode, alloc: *Allocator) Self {
+        return Self{
+            .mem = Mem.init(intcode, alloc),
+            .pc = 0,
+            .relative_base = 0,
+            .state = State.Running,
+            .input = null,
+            .output = null,
+        };
+    }
+
+    fn getParam(self: Self, n: usize, mode: Instr) !Instr {
+        const val = self.mem.get(self.pc + 1 + n);
+        return switch (mode) {
+            0 => self.mem.get(@intCast(usize, val)),
+            1 => val,
+            2 => self.mem.get(@intCast(usize, self.relative_base + val)),
+            else => return error.InvalidParamMode,
+        };
+    }
+
+    fn getPos(self: Self, n: usize, mode: Instr) !usize {
+        const val = self.mem.get(self.pc + 1 + n);
+        return switch (mode) {
+            0 => @intCast(usize, val),
+            1 => return error.OutputParameterImmediateMode,
+            2 => @intCast(usize, self.relative_base + val),
+            else => return error.InvalidParamMode,
+        };
+    }
+
+    pub fn exec(self: *Self) !void {
+        const instr = self.mem.get(self.pc);
+        switch (opcode(instr)) {
+            99 => self.state = State.Stopped,
+            1 => {
+                const val_x = try self.getParam(0, paramMode(instr, 0));
+                const val_y = try self.getParam(1, paramMode(instr, 1));
+                const pos_result = try self.getPos(2, paramMode(instr, 2));
+                try self.mem.set(pos_result, val_x + val_y);
+                self.pc += 4;
+            },
+            2 => {
+                const val_x = try self.getParam(0, paramMode(instr, 0));
+                const val_y = try self.getParam(1, paramMode(instr, 1));
+                const pos_result = try self.getPos(2, paramMode(instr, 2));
+                try self.mem.set(pos_result, val_x * val_y);
+                self.pc += 4;
+            },
+            3 => {
+                const pos_x = try self.getPos(0, paramMode(instr, 0));
+                if (self.input) |val| {
+                    try self.mem.set(pos_x, val);
+                    self.pc += 2;
+                    self.input = null;
+                } else {
+                    self.state = State.AwaitingInput;
+                }
+            },
+            4 => {
+                const val_x = try self.getParam(0, paramMode(instr, 0));
+                if (self.output) |_| {
+                    self.state = State.AwaitingOutput;
+                } else {
+                    self.output = val_x;
+                    self.pc += 2;
+                }
+            },
+            5 => {
+                const val_x = try self.getParam(0, paramMode(instr, 0));
+                if (val_x != 0) {
+                    const val_y = try self.getParam(1, paramMode(instr, 1));
+                    self.pc = @intCast(usize, val_y);
+                } else {
+                    self.pc += 3;
+                }
+            },
+            6 => {
+                const val_x = try self.getParam(0, paramMode(instr, 0));
+                if (val_x == 0) {
+                    const val_y = try self.getParam(1, paramMode(instr, 1));
+                    self.pc = @intCast(usize, val_y);
+                } else {
+                    self.pc += 3;
+                }
+            },
+            7 => {
+                const val_x = try self.getParam(0, paramMode(instr, 0));
+                const val_y = try self.getParam(1, paramMode(instr, 1));
+                const pos_result = try self.getPos(2, paramMode(instr, 2));
+                try self.mem.set(pos_result, if (val_x < val_y) 1 else 0);
+                self.pc += 4;
+            },
+            8 => {
+                const val_x = try self.getParam(0, paramMode(instr, 0));
+                const val_y = try self.getParam(1, paramMode(instr, 1));
+                const pos_result = try self.getPos(2, paramMode(instr, 2));
+                try self.mem.set(pos_result, if (val_x == val_y) 1 else 0);
+                self.pc += 4;
+            },
+            9 => {
+                const val_x = try self.getParam(0, paramMode(instr, 0));
+                self.relative_base += val_x;
+                self.pc += 2;
+            },
+            else => {
+                std.debug.warn("pos: {}, instr: {}\n", .{ self.pc, instr });
+                return error.InvalidOpcode;
+            },
+        }
+    }
+
+    pub fn execUntilHalt(self: *Self) !void {
+        // try to jump-start the computer if it was waiting for I/O
+        if (self.state != State.Stopped)
+            self.state = State.Running;
+
+        while (self.state == State.Running)
+            try self.exec();
+    }
+};
+
+test "test exec 1" {
+    var intcode = [_]Instr{ 1, 0, 0, 0, 99 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    try comp.execUntilHalt();
+    expectEqual(intcode[0], 2);
+}
+
+test "test exec 2" {
+    var intcode = [_]Instr{ 2, 3, 0, 3, 99 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    try comp.execUntilHalt();
+    expectEqual(intcode[3], 6);
+}
+
+test "test exec 3" {
+    var intcode = [_]Instr{ 2, 4, 4, 5, 99, 0 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    try comp.execUntilHalt();
+    expectEqual(intcode[5], 9801);
+}
+
+test "test exec with different param mode" {
+    var intcode = [_]Instr{ 1002, 4, 3, 4, 33 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    try comp.execUntilHalt();
+    expectEqual(intcode[4], 99);
+}
+
+test "test exec with negative integers" {
+    var intcode = [_]Instr{ 1101, 100, -1, 4, 0 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    try comp.execUntilHalt();
+    expectEqual(intcode[4], 99);
+}
+
+test "test equal 1" {
+    var intcode = [_]Instr{ 3,9,8,9,10,9,4,9,99,-1,8 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    comp.input = 8;
+    try comp.execUntilHalt();
+    expectEqual(comp.output.?, 1);
+}
+
+test "test equal 2" {
+    var intcode = [_]Instr{ 3,9,8,9,10,9,4,9,99,-1,8 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    comp.input = 13;
+    try comp.execUntilHalt();
+    expectEqual(comp.output.?, 0);
+}
+
+test "test less than 1" {
+    var intcode = [_]Instr{ 3,9,7,9,10,9,4,9,99,-1,8 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    comp.input = 5;
+    try comp.execUntilHalt();
+    expectEqual(comp.output.?, 1);
+}
+
+test "test less than 2" {
+    var intcode = [_]Instr{ 3,9,7,9,10,9,4,9,99,-1,8 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    comp.input = 20;
+    try comp.execUntilHalt();
+    expectEqual(comp.output.?, 0);
+}
+
+test "test equal immediate" {
+    var intcode = [_]Instr{ 3,3,1108,-1,8,3,4,3,99 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    comp.input = 8;
+    try comp.execUntilHalt();
+    expectEqual(comp.output.?, 1);
+}
+
+test "test less than immediate" {
+    var intcode = [_]Instr{ 3,3,1107,-1,8,3,4,3,99 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    comp.input = 3;
+    try comp.execUntilHalt();
+    expectEqual(comp.output.?, 1);
+}
+
+test "quine" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
+    const allocator = &arena.allocator;
+    defer arena.deinit();
+
+    var intcode = [_]Instr{ 109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99 };
+    var comp = IntcodeComputer.init(&intcode, allocator);
+    try comp.execUntilHalt();
+}
+
+test "big number" {
+    var intcode = [_]Instr{ 1102,34915192,34915192,7,4,7,99,0 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    try comp.execUntilHalt();
+}
+
+test "big number 2" {
+    var intcode = [_]Instr{ 104,1125899906842624,99 };
+    var comp = IntcodeComputer.init(&intcode, std.testing.failing_allocator);
+    try comp.execUntilHalt();
+
+    expectEqual(comp.output.?, 1125899906842624);
+}
+
+fn pointAffected(alloc: *Allocator, ints: []const Instr, x: Instr, y: Instr) !bool {
+    var ints_cpy = try std.mem.dupe(alloc, Instr, ints);
+    defer alloc.free(ints_cpy);
+
+    var comp = IntcodeComputer.init(ints_cpy, alloc);
+
+    try comp.execUntilHalt();
+    comp.input = x;
+
+    try comp.execUntilHalt();
+    comp.input = y;
+
+    try comp.execUntilHalt();
+    const res = comp.output orelse return error.NoOutput;
+    comp.output = null;
+
+    return res == 1;
+}
+
+fn findLowerEdge(alloc: *Allocator, ints: []const Instr, x: Instr) !Instr {
+    var in_beam = false;
+    var y: Instr = 0;
+
+    while (true) : (y += 1) {
+        const affected = try pointAffected(alloc, ints, x, y);
+        if (in_beam) {
+            if (!affected) break;
+        } else {
+            if (affected) in_beam = true;
+        }
+    }
+
+    return y - 1;
+}
+
+fn squareFits(alloc: *Allocator, ints: []const Instr, x: Instr) !bool {
+    const lower_edge = try findLowerEdge(alloc, ints, x);
+    if (lower_edge >= 100) {
+        if (try pointAffected(alloc, ints, x + 99, lower_edge - 99)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
+    const allocator = &arena.allocator;
+    defer arena.deinit();
+
+    const input_file = try std.fs.cwd().openFile("input19.txt", .{});
+    var input_stream = input_file.inStream();
+    var buf: [1024]u8 = undefined;
+    var ints = std.ArrayList(Instr).init(allocator);
+
+    // read amp intcode into an int arraylist
+    while (try input_stream.readUntilDelimiterOrEof(&buf, ',')) |item| {
+        // add an empty element to the input file because I don't want to modify
+        // this to discard newlines
+        try ints.append(std.fmt.parseInt(Instr, item, 10) catch 0);
+    }
+
+    std.debug.warn("le for 4: {}\n", .{ try findLowerEdge(allocator, ints.toSlice(), 40) });
+
+    // scan area very coarse-grained
+    var x: Instr = 10;
+    while (true) : (x *= 2) {
+        if (try squareFits(allocator, ints.toSlice(), x)) break;
+    }
+
+    // binary search to find exact spot
+    var hi = x;
+    var lo = @divTrunc(x, 2);
+    var mid: Instr = undefined;
+
+    while (hi >= lo) {
+        mid = lo + @divTrunc(hi - lo, 2);
+        if (try squareFits(allocator, ints.toSlice(), mid)) {
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
+    std.debug.assert(try squareFits(allocator, ints.toSlice(), lo));
+    std.debug.assert(!try squareFits(allocator, ints.toSlice(), hi));
+
+    const res_x = lo;
+    const res_y = (try findLowerEdge(allocator, ints.toSlice(), res_x)) - 99;
+
+    std.debug.warn("result: x={} y={} n={}\n", .{ res_x, res_y, res_x * 10000 + res_y });
+}
